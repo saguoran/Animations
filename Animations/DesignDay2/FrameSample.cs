ca2f12c87e6e37797c7f9bbd7ae6cae481d165d2
saguoran <saguoran@gmail.com>
2019-07-19 21:56:31 -0400

    Image button control testm

---
+++ Animations/DesignDay2/FrameSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Animations.DesignDay2
+{
+    public class FrameSample : ContentPage
+    {
+        public FrameSample()
+        {
+            Label header = new Label
+            {
+                Text = "Frame",
+                FontSize = 50,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Frame frame = new Frame
+            {
+                BorderColor = Color.Accent,
+                HasShadow = true,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Content = new Label
+                {
+                    Text = "I've been framed!"
+                }
+            };
+
+            // Build the page.
+            Title = "Frame Demo";
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    frame
+                }
+            };
+        }
+    }
+}
---
+++ Animations/DesignDay2/ImageButton.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Animations.DesignDay2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageButton : ContentView
+    {
+        public static readonly BindableProperty ButtonTextProperty =
+            BindableProperty.Create("ButtonText", typeof(string), typeof(ImageButton), default(string));
+
+        public string ButtonText {
+            get { return GetValue(ButtonTextProperty) as string; }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        public event EventHandler Clicked;
+
+        public static readonly BindableProperty CommandProperty =
+           BindableProperty.Create("Command", typeof(ICommand), typeof(ImageButton), null);
+
+        public ICommand Command
+        {
+            get => GetValue(CommandProperty) as ICommand; 
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly BindableProperty CommandParameterProperty =
+                BindableProperty.Create("CommandParameter", typeof(object), typeof(ImageButton), null);
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty) as object;
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly BindableProperty ImageSourceProperty =
+            BindableProperty.Create("Source", typeof(ImageSource), typeof(ImageButton), default(ImageSource));
+
+        public ImageSource Source {
+            get => GetValue(ImageSourceProperty) as ImageSource;
+            set { SetValue(ImageSourceProperty, value); }
+        }
+        public ImageButton()
+        {
+            InitializeComponent();
+
+            InnerLabel.SetBinding(Label.TextProperty, new Binding("ButtonText", source: this));
+            InnerImage.SetBinding(Image.SourceProperty, new Binding("Source", source: this));
+
+        }
+    }
+}
