030a22e39633881b9d429069640323fea4edbe41
saguoran <saguoran@gmail.com>
2019-07-21 02:22:46 -0400

    TabView functional finished

--- Animations/DesignDay2/TappedView2rdTry.xaml.cs
+++ Animations/DesignDay2/TappedView2rdTry.xaml.cs
@@ -12,30 +12,60 @@ namespace Animations.DesignDay2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TappedView2rdTry : ContentView
     {
-        Random rand = new Random();
-        int padding = 3;
+        private static string selected;
+        private static List<Note> notes = Note.NoteResource();
         public TappedView2rdTry()
         {
             InitializeComponent();
-            //int i = 0;
-            //int max = 20;
-            //stackLayout.WidthRequest = max * 200 * 2 * 5;
-            ////var stackLayout = new StackLayout {Orientation=StackOrientation.Horizontal};
-            //while (i< max)
-            //{
-            //    int b = (int)rand.NextDouble()*10;
-            //    string color = $"f{b}f";
-            //    i += 1;
-            //    var boxView = new BoxView
-            //    {
-            //        BackgroundColor = Color.FromHex(color),
-            //        WidthRequest = 200,
-            //        HeightRequest = 60,
-            //        Margin = new Thickness(5, 0, 5, 0),
-            //    };
-            //    stackLayout.Children.Add(boxView);
-            //}
-            //scrollView.Content = stackLayout;
+            var tapRecognizer = new TapGestureRecognizer();
+            notes.ForEach(note =>
+            {
+                TabeContainer.Children.Add(new Label
+                {
+                    Text = note.Title,
+                    WidthRequest = 200,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    BackgroundColor = Color.Aqua,
+                    Margin = new Thickness(0.5, 0),
+                    ClassId = note.Id,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                    GestureRecognizers = { tapRecognizer }
+                }
+                );
+            });
+            tapRecognizer.Tapped += (o, args) => {
+                Label selectedLabel = TabeContainer.Children.Where(l => l.ClassId == selected).FirstOrDefault() as Label;
+                selectedLabel.BackgroundColor = Color.Aqua;
+                Label label = o as Label;
+                selected = label.ClassId;
+                label.BackgroundColor = Color.Accent;
+                editorNoUnderline.Text = notes.Find(note => note.Id == label.ClassId).Detail;
+            };
+            selected = notes[0].Id;
+            TabeContainer.Children[0].BackgroundColor = Color.Accent;
+        }
+
+        //hardcode function
+    }
+    public class Note
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        private static List<Note> Notes = new List<Note>();
+        public static List<Note> NoteResource()
+        {
+            Notes.Add(new Note { Id = "0", Title = "Tab 1", Detail = "LKjflaksdjfkljasdfdasfasd" });
+            Notes.Add(new Note { Id = "1", Title = "Tab 2", Detail = "gfgasgdadfasdfasdaksdjfkljasdfdasfasd" });
+            Notes.Add(new Note { Id = "2", Title = "Tab 3", Detail = "qewrretsdfdasfasdfasdasfasd" });
+            Notes.Add(new Note { Id = "3", Title = "Tab 4", Detail = "jyngjhgqewasdfrretsdfdasfasdfasdasfasd" });
+            Notes.Add(new Note { Id = "4", Title = "Tab 5", Detail = "asdfqasdfaewrretsdfdasfasdfasdasfasd" });
+            return Notes;
         }
     }
 }
+
+
+
+
